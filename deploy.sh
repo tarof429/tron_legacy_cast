1366886058bf0a4e83da23ba65dd877e6302ece0
Taro Fukunaga <tarof429@gmail.com>
2020-10-08 13:18:54 -0700

    Delete locally built images

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,9 @@ echo "Building git revision ${SHA}"
 
 docker build -t tarof429/tron_legacy_cast:latest api_client -t tarof429/tron_legacy_cast:${SHA}
 
-echo "Pushing image to dockerhub"
+echo "Pushing images to dockerhub"
 docker push tarof429/tron_legacy_cast:latest
-docker push  tarof429/tron_legacy_cast:${SHA}
+docker push tarof429/tron_legacy_cast:${SHA}
+
+echo "Cleaning up images"
+docker rmi tarof429/tron_legacy_cast:latest api_client -t tarof429/tron_legacy_cast:${SHA}
